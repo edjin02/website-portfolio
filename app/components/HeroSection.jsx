b429bdc8668fca8cfc6cee154ee576ef67f2ee89
Earl <earlybauan1@gmail.com>
2023-11-30 23:51:23 +0800

    apostrophe added

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
             >
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal  font-extrabold">
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-secondary-400 to-primary-400"> 
-                Hello, Im {" "} 
+                Hello, I'm {" "} 
                 </span>
                 <br></br>
                 <TypeAnimation
